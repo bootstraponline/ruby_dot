45879f4b23770069d8e3f76cb77d6a5c775f639c
Matthew Edwards <medwards@instructure.com>
2017-02-26 20:09:09 -0500

    Render to grid

--- lib/ruby_dot/render.rb
+++ lib/ruby_dot/render.rb
@@ -6,30 +6,48 @@ module RubyDot
     def draw_io_xml hash
       file_array = []
 
-      id = 1
-      x = 10
-      y = 10
-      width = 120
-      _height = 60
+      id     = 1
+      width  = 180
+      height = 60
 
-      space_x = x + width/2.0
+      space_x = 10 + width
+      space_y = 10 + height
+
+      row_limit = 4
+      row       = 0 # x
+      column    = 0 # y
 
       hash.each do |key, value|
         file_name = key
         modules   = Array(value[:modules])
         classes   = Array(value[:classes])
 
-        # value is rendered without escaping using {{{ }}} in the mustache template
+        if row >= row_limit
+          row    = 0
+          column += 1
+        end
 
+        # lay out the nodes in a 4x row grid
+        #
+        # x x x x
+        # x x x x
+        # x x x x
+        x                = row * space_x
+        y                = column * space_y
+
+        # value is rendered without escaping using {{{ }}} in the mustache template
         file_description = [modules + classes]
         file_array << {
-            id: id+=1,
-            value: "&lt;b&gt;#{file_name}&lt;/b&gt;&lt;div&gt;#{file_description.join "\n"}&lt;/div&gt;",
-            x: id * space_x,
-            y: y
+            id:     id+=1,
+            value:  "&lt;b&gt;#{file_name}&lt;/b&gt;&lt;div&gt;#{file_description.join "\n"}&lt;/div&gt;",
+            x:      x,
+            y:      y,
+            width:  width,
+            height: height
         }
 
         id += 1
+        row += 1
       end
 
       Mustache.render File.read(DRAW_IO), file_array: file_array
